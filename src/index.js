c5f220cc2c174d57c021ee138ac5d71fcb41ac0d
Devin Mounts <devinmounts@gmail.com>
2018-12-04 17:16:18 -0800

    register Firebase provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App />,
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
   document.getElementById('root'));
 
 
