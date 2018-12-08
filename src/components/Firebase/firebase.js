41a6a09e36a82738a1048abfa1e654acd3e2de95
Devin Mounts <devinmounts@gmail.com>
2018-12-07 16:26:23 -0800

    init firebase db api.

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
-import * as ROUTES from '../../constants/routes'
+import 'firebase/database';
 
 const prodConfig = {
   apiKey: process.env.REACT_APP_PROD_API_KEY,
@@ -28,8 +28,7 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
-
-    
+    this.db = app.database();    
   }
   
   //** Auth API */
@@ -48,6 +47,12 @@ class Firebase {
 
   doPasswordUpdate = password =>
     this.auth.currentUser.updatePassword(password);
+
+  //** User API */
+
+  user = uid => this.db.ref(`users/${uid}`);
+
+  users = () => this.db.ref('users');
 }
 
 export default Firebase;
