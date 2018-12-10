ab62fdf56755be70599ba96592943bd9b67d2fb2
Devin Mounts <devinmounts@gmail.com>
2018-12-10 08:35:01 -0800

    istantiate actions.

--- src/reducers/userAuthReducer.js
+++ src/reducers/userAuthReducer.js
@@ -1,10 +1,14 @@
-// import constants from '../constants';
-// const { types, INITIAL_STATE} = constants
-// import
+import constants from '../constants';
+const { types, INITIAL_STATE} = constants
+import
 
-// const userAuthReducer = (state = INITIAL_STATE.authUser, action) => {
-//   switch(action.type) {
-//     case types.CREATE_USER_WITH_EMAIL_AND_PASSWORD:
-//       return
-//   }
-// }
+const userAuthReducer = (state = INITIAL_STATE.authUser, action) => {
+  switch(action.type) {
+    case types.CREATE_USER_WITH_EMAIL_AND_PASSWORD:
+      return
+    default:
+      return state;
+  }
+};
+
+export default userAuthReducer;
