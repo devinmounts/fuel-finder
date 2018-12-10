fe9533eedbb3161181e3c35a13dd7b2d334efffa
Devin Mounts <devinmounts@gmail.com>
2018-12-09 16:10:57 -0800

    alter admin view.

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import { withFirebase } from '../Firebase';
+import { withAuthentication } from '../Session'; 
+import { compose } from 'recompose';
 
 const SignOutButton = ({ firebase }) => (
- <button type='button' onClick={firebase.doSignOut}>
-  Sign Out
- </button>
+  <button type='button' onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
 );
 
-export default withFirebase(SignOutButton);
+export default compose(
+  withFirebase,
+  withAuthentication
+)(SignOutButton);
