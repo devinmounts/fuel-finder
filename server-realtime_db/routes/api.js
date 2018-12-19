b857498d4e72ee0962a3b35373233fbf4a91b19a
Devin Mounts <devinmounts@gmail.com>
2018-12-19 14:56:47 -0800

    open app on port 900 and create watcher for changes in messages collection.

--- server-realtime_db/routes/api.js
+++ server-realtime_db/routes/api.js
@@ -15,4 +15,21 @@ router.post('/new', (req, res) => {
   });
 });
 
-router.
+router.route('/:id')
+  //** DELETE */
+  .delete((req, res) => {
+    Message.findById(req.params.id, (err, message) => {
+      if (err) {
+        console.log('DELETE ERROR: ' + err);
+        res.status(500).send('Error');
+      } else if (message) {
+        task.remove( () => {
+          res.status(200).json(message);
+        });
+      } else {
+        res.status(404).send('Not found');
+      }
+    });
+  });
+
+  module.exports = router;
---
+++ server-realtime_db/server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const api = require('./routes/api');
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: '676000',
+  key: 'cfac003dbca7d9106af7',
+  secret: '74aed2ff596027dac5f2',
+  cluster: 'us2',
+  encrypted: true
+});
+
+const channel = 'messages';
+
+const app = express();
+
+app.user((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', api);
+
+mongoose.connect('mongodb://localhost/fuel_finder?replicaSet=rs');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Connection Error"));
+
+db.once('open', () => {
+  app.listen(9000, () => {
+    console.log('Node server running on port 9000');
+  });
+
+  const messagesCollection = db.collection('messages');
+  const changeStream = messagesCollection.watch();
+
+  changeStream.on('change', (change) => {
+    console.log(change);
+
+    if(change.operationType === 'insert') {
+      const message = change.fullDocument;
+      pusher.trigger(
+        channel,
+        'inserted',
+        {
+          id: message._id,
+          message: message.message
+        }
+      );
+    } else if(change.operationType === 'delete') {
+      pusher.trigger(
+        channel,
+        'deleted',
+        change.documentKey._id
+      );
+    }
+  });
+});
