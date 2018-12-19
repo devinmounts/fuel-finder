0ef6e907af422789dcdc2ca2c13a9f62850c8b87
Devin Mounts <devinmounts@gmail.com>
2018-12-19 10:30:13 -0800

    add basic style to nav

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './styles.css';
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
- <button type='button' onClick={firebase.doSignOut}>
+ <button className='sign-out-button' type='button' onClick={firebase.doSignOut}>
   Sign Out
  </button>
 );
