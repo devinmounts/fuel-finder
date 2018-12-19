044b4419947cfe0da9345b3d586137f608917d3e
Devin Mounts <devinmounts@gmail.com>
2018-12-19 11:01:04 -0800

    resolve head on signout

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -5,21 +5,10 @@ import { withAuthentication, withAuthorization } from '../Session';
 import { compose } from 'recompose';
 
 const SignOutButton = ({ firebase }) => (
-<<<<<<< HEAD
-<<<<<<< HEAD
-  <button type='button' onClick={firebase.doSignOut}>
-    Sign Out
-  </button>
-=======
  <button className='sign-out-button' type='button' onClick={firebase.doSignOut}>
   Sign Out
  </button>
->>>>>>> redux
-=======
- <button className='sign-out-button' type='button' onClick={firebase.doSignOut}>
-  Sign Out
- </button>
->>>>>>> 619d9d05242c22444a0a36198fbdc3a57a24f4cb
+
 );
 
 export default compose(
